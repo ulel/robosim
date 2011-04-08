0e2383569f3b90bfd092fe42708a5ec8283f4cfc
pier@student.gu.se <pier@student.gu.se@2e70b80b-895d-58bd-b249-86f46f8390c5>
2011-04-08 18:30:20 +0000

    Refactoring

--- RoboSim/src/sumosim/robot/Robot.java
+++ RoboSim/src/sumosim/robot/Robot.java
@@ -1,173 +1,45 @@
 package sumosim.robot;
 
-import java.awt.geom.AffineTransform;
-
-import sumosim.SumoRobot;
-import sumosim.robot.sensors.*;
-import net.phys2d.math.Vector2f;
-import net.phys2d.raw.Body;
-import net.phys2d.raw.FixedJoint;
+import sumosim.robot.components.RobotHull;
 import net.phys2d.raw.World;
-import net.phys2d.raw.shapes.Box;
 
+/**
+ * The Robot class provides an abstract baseclass for all robots.
+ */
+public abstract class Robot {
 
-public class Robot {
-	public static final String ROBOT_HULL = "RobotHull";
-	public static final long ROBOT_HULL_BITMASK = 5; //bit 1 and 3.
-	public static final String ROBOT_WHEEL = "RobotWheel";
-	public static final long ROBOT_WHEEL_BITMASK = 8; //bit 4.
+	protected World world;
+	private RobotHull hull;
 	
-	private World world;
-	private Body hull;
-	private Body wheel1;
-	private Body wheel2;
-	
-	public ProximitySensor sensorA;
-	public ContactSensor sensorB;
-	public GroundSensor sensorC;
-	
-	private float force = 200;
-	
-	//TODO fix rotation parameter: only works for 0f, partially for Pi, not at all for anything else.
-	public Robot(World world, float posX, float posY, float rotation, float mass) {
-		this.world = world;
-		
-		hull = new Body(Robot.ROBOT_HULL, new Box(30, 30), mass);
-		hull.setBitmask(ROBOT_HULL_BITMASK);
-		hull.setFriction(0.2f);
-		
-		wheel1 = new Body(Robot.ROBOT_WHEEL, new Box(5, 15), 1);
-		wheel1.setBitmask(ROBOT_WHEEL_BITMASK);
-		wheel1.setDamping(0.1f);
+	public Robot(World w, float posX, float posY, float rotation, float mass) {
+		this.world = w;
+		this.hull = new RobotHull(w, this, posX, posY, rotation, mass);
+	}
 
-		wheel2 = new Body(Robot.ROBOT_WHEEL, new Box(5, 15), 1);
-		wheel2.setBitmask(ROBOT_WHEEL_BITMASK);
-		wheel2.setDamping(0.1f);
-		
-		AffineTransform t = new AffineTransform();
-		t.translate(posX, posY);
-		hull.setPosition((float)t.getTranslateX(), (float)t.getTranslateY());
-		hull.setRotation(rotation);
-		
-		t = new AffineTransform();
-		t.translate(posX, posY);
-		t.rotate(rotation);
-		t.translate(-10, 0);
-		wheel1.setPosition((float)t.getTranslateX() , (float)t.getTranslateY());
-		wheel1.setRotation(rotation);
-		
-		t = new AffineTransform();
-		t.translate(posX, posY);
-		t.rotate(rotation);
-		t.translate(10, 0);
-		wheel2.setPosition((float)t.getTranslateX(), (float)t.getTranslateY());
-		wheel2.setRotation(rotation);
-		
-		world.add(hull);
-		world.add(wheel1);
-		world.add(wheel2);
-		
-		world.add(new FixedJoint(wheel1, wheel2));
-		world.add(new FixedJoint(hull, wheel1));
-		world.add(new FixedJoint(hull, wheel2));
-		
-		
-		this.sensorA = new ProximitySensor(world, this);
-		this.sensorA.setHRange(20);
-		this.sensorA.setVRange(60);
-		this.sensorA.setPosition(0, 9);
-		this.sensorA.removeBit(SumoRobot.DOHYO_ARENA_BITMASK);
-		this.sensorA.addExcludedBody(this.hull);
-		
-		this.sensorB = new ContactSensor(world, this);
-		this.sensorB.setRotation((float)Math.PI);
-		this.sensorB.setPosition(0, -15);
-		this.sensorB.setSensorShape(new Box(15, 2.5f));
-		this.sensorB.removeBit(SumoRobot.DOHYO_ARENA_BITMASK);
-		this.sensorB.addExcludedBody(this.hull);
-		
-		this.sensorC = new GroundSensor(world, this, SumoRobot.DOHYO_ARENA_BITMASK);
-		
-	}
+	/**
+	 * Returns the x position of the robot.
+	 */
+	public float getPosX() { return this.hull.getWorldPosX(); }
 	
-	public float getPosX() {
-		return this.hull.getPosition().getX();
-	}
-	public float getPosY() {
-		return this.hull.getPosition().getY();
-	}
-	public float getRotation() {
-		return this.hull.getRotation();
-	}
+	/**
+	 * Returns the x position of the robot.
+	 */
+	public float getPosY() { return this.hull.getWorldPosY(); }
 	
-	public Body getHull() {
-		return this.hull;
-	}
+	/**
+	 * Returns the rotation of the robot.
+	 * @return
+	 */
+	public float getRotation() { return this.hull.getWorldRotation(); }
 	
-	
-	private Vector2f getForce() {
-		float r = hull.getRotation();
-		float x = (float) (Math.sin(r) * force);
-		float y = (float) (Math.cos(r) * force);
-		
-		return new Vector2f(x,y);
-	}
-	
-	public void forward() {
-		Vector2f force = getForce();
-		wheel1.addForce(new Vector2f(-force.x,force.y));
-		wheel2.addForce(new Vector2f(-force.x,force.y));
-	}
-	
-	public void backward() {
-		Vector2f force = getForce();
-		wheel1.addForce(new Vector2f(force.x,-force.y));
-		wheel2.addForce(new Vector2f(force.x,-force.y));
-	}
-	
-	public void turnClockwise() {
-		Vector2f force = getForce();
-		wheel1.addForce(new Vector2f(force.x,-force.y));
-		wheel2.addForce(new Vector2f(-force.x,force.y));
-	}
-	
-	public void turnCounterClockwise() {
-		Vector2f force = getForce();
-		wheel1.addForce(new Vector2f(-force.x,force.y));
-		wheel2.addForce(new Vector2f(force.x,-force.y));
-	}
-	
-	
-	
-	
-	private int missileFired = 61;
-	public void fireMissile() {
-		if (this.missileFired < 60)
-		{
-			this.missileFired++;
-			return;
-		}
-		
-		Body missileBody = new Body("missile", new Box(5, 20), 350);
-		missileBody.setPosition(this.getPosX(), this.getPosY());
-		missileBody.setRotation(this.getRotation());
-		
-		hull.addExcludedBody(missileBody);
-		wheel1.addExcludedBody(missileBody);
-		wheel2.addExcludedBody(missileBody);
-		world.add(missileBody);
-		
-		missileBody.addForce(new Vector2f(0, (int)Math.pow(10, 6.5)));
-		
-		this.missileFired = 0;
-	}
+	/**
+	 * Returns the robot's hull.
+	 */
+	public RobotHull getHull() { return this.hull; }
 	
 	
 	/**
-	 * The step method is called by the simulator for each step.
-	 * Robot behavior can be defined in this method.
+	 * This method is called for each simulation step and can be overridden to add robot behavior.
 	 */
-	public void step() {
-		
-	}
+	public void step() {}
 }
